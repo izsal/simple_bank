1aa44612251039b70a60be321c350daa8476ebee
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2024-02-09 08:52:34 +0700

    feat: 🎸 get one and list in golang

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,4 +1,4 @@
--- name: CreateAuthor :one
+-- name: CreateAccount :one
 INSERT INTO accounts (
   owner,
   balance,
@@ -7,3 +7,13 @@ INSERT INTO accounts (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccount :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -9,7 +9,7 @@ import (
 	"context"
 )
 
-const createAuthor = `-- name: CreateAuthor :one
+const createAccount = `-- name: CreateAccount :one
 INSERT INTO accounts (
   owner,
   balance,
@@ -20,14 +20,14 @@ INSERT INTO accounts (
 RETURNING id, owner, balance, currency, created_at
 `
 
-type CreateAuthorParams struct {
+type CreateAccountParams struct {
 	Owner    string
 	Balance  int64
 	Currency string
 }
 
-func (q *Queries) CreateAuthor(ctx context.Context, arg CreateAuthorParams) (Account, error) {
-	row := q.db.QueryRow(ctx, createAuthor, arg.Owner, arg.Balance, arg.Currency)
+func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error) {
+	row := q.db.QueryRow(ctx, createAccount, arg.Owner, arg.Balance, arg.Currency)
 	var i Account
 	err := row.Scan(
 		&i.ID,
@@ -37,4 +37,60 @@ func (q *Queries) CreateAuthor(ctx context.Context, arg CreateAuthorParams) (Acc
 		&i.CreatedAt,
 	)
 	return i, err
+}
+
+const getAccount = `-- name: GetAccount :one
+SELECT id, owner, balance, currency, created_at FROM accounts
+WHERE id = $1 LIMIT 1
+`
+
+func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
+	row := q.db.QueryRow(ctx, getAccount, id)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
+const listAccount = `-- name: ListAccount :many
+SELECT id, owner, balance, currency, created_at FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2
+`
+
+type ListAccountParams struct {
+	Limit  int32
+	Offset int32
+}
+
+func (q *Queries) ListAccount(ctx context.Context, arg ListAccountParams) ([]Account, error) {
+	rows, err := q.db.Query(ctx, listAccount, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Account
+	for rows.Next() {
+		var i Account
+		if err := rows.Scan(
+			&i.ID,
+			&i.Owner,
+			&i.Balance,
+			&i.Currency,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
