ac1f97d41613ff27439ae1cb253c25a9e116fa74
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2024-05-04 16:47:11 +0700

    add validator for when send money

--- api/validator.go
+++ api/validator.go
@@ -5,6 +5,9 @@ import (
 	"github.com/izsal/simple_bank/util"
 )
 
+// This code snippet is defining a custom validation function named `validCurrency` using the Go
+// Playground Validator library. The function takes a `validator.FieldLevel` parameter and returns a
+// boolean value.
 var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
 	if currency, ok := fieldLevel.Field().Interface().(string); ok {
 		return util.IsSupportedCurrency(currency)
